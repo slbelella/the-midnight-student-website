20fa16810071808376ddc1e85c794577584a5c3a
slbelella <101361155+slbelella@users.noreply.github.com>
2022-03-10 14:53:47 -0500

    Create albums.php

---
+++ albums.php
@@ -0,0 +1,88 @@
+<?php
+$title = "The Midnight Albums";
+require 'templates/header.html';
+?>
+
+<link href="style.css" rel="stylesheet">
+<link rel="stylesheet" href="https://use.typekit.net/ony3qws.css">
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="description" content="The Midnight Albums">
+<title><?php echo $title?></title>
+</head>
+<body>
+<h2>Listen Now on Spotify</h2>
+
+<div class="albums">
+  <a href="https://open.spotify.com/album/54xM746igJkdPJenRhW1za"><figure class="snip1573">
+    <img src="images/daysofthunder.jpg" width="160" height="160" alt="Days of Thunder" />
+    <figcaption>
+      <h3>Days of Thunder</h3>
+    </figcaption>
+  </figure></a>
+
+  <a href="https://open.spotify.com/album/4Krg8zvprquh7TVn9OxZn8"><figure class="snip1573">
+      <img src="images/endlesssummer.jpg" width="160" height="160" alt="Endless Summer" />
+    <figcaption>
+      <h3>Endless Summer</h3>
+    </figcaption>
+    
+  </figure></a>
+
+  <a href="https://open.spotify.com/album/7gcGPeI9nHWPd9K0JNE7OH"><figure class="snip1573">
+      <img src="images/remixed1.jpg" width="160" height="160" alt="The Midnight Remixed" />
+    <figcaption>
+      <h3>The Midnight Remixed</h3>
+    </figcaption>
+    
+  </figure></a>
+
+  <a href="https://open.spotify.com/album/42sLDZoNRGO3YnTgpAbk9o"><figure class="snip1573">
+    <img src="images/nocturnal.jpg" width="160" height="160" alt="Nocturnal" />
+    <figcaption>
+      <h3>Nocturnal</h3>
+    </figcaption>
+    
+  </figure></a>
+
+  <a href="https://open.spotify.com/album/49ItLbfOzgYIVOeIdxxrnn"><figure class="snip1573">
+    <img src="images/kids.jpg" width="160" height="160" alt="Kids" />
+    <figcaption>
+      <h3>Kids</h3>
+    </figcaption>
+    
+  </figure></a>
+
+  <a href="https://open.spotify.com/album/57aUtDHSulUS0D5r8nnO6V"><figure class="snip1573">
+    <img src="images/remixed2.jpg" width="160" height="160" alt="The Midnight Remixed 2" />
+    <figcaption>
+      <h3>The Midnight Remixed 2</h3>
+    </figcaption>
+    
+  </figure></a>
+
+  <a href="https://open.spotify.com/album/1WKMbxcldmwnXaCIGgEpUW"><figure class="snip1573">
+    <img src="images/monsters.jpg" width="160" height="160" alt="Monsters" />
+    <figcaption>
+      <h3>Monsters</h3>
+    </figcaption>
+    
+  </figure></a>
+
+  <a href="https://open.spotify.com/album/2Qse49swFybD2LtrWM5kTq?autoplay=true"><figure class="snip1573">
+    <img src="images/horrorshow.jpg" width="160" height="160" alt="Horror Show" />
+    <figcaption>
+      <h3>Horror Show</h3>
+    </figcaption>
+    
+  </figure></a>
+  </div>
+
+        <img src="images/pizza.jpg" class="pizza" alt="Days of Thunder" style="width:100%">
+
+
+        <?php
+include('templates/footer.html');
+?>
